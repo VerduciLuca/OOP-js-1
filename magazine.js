46308c33ce7b8e68c34f6da3289b378bf773b418
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-03 11:54:02 +0200

    commit

---
+++ magazine.js
@@ -0,0 +1,8 @@
+class Magazine{
+    constructor(title, dop, publisher, issue){
+        this.title = title;
+        this.dop = dop;
+        this.publisher = publisher;
+        this.issue = issue;
+    }
+}
