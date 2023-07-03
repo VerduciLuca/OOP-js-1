5b8cae22bd57a1b1c064b0942aa49597fe7daa9c
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-03 12:25:00 +0200

    Update book.js

--- book.js
+++ book.js
@@ -1,20 +1,12 @@
 class Book{
     constructor(title, author, dop, publisher){
-        this.title = title;
+        super(title, dop, publisher)
         this.author = author;
-        this.dop = dop;
-        this.publisher = publisher;
     }
 
     toString() {
-        const card = 'Titolo: ' + this.title + '\n' + 'Autore: ' + this.author + '\n' + 'Data di pubblicazione: ' + this.dop + '\n' + 'Casa editrice: ' + this.publisher + '\n';
+        const card = super.toString() + 'Autore: ' + this.author + '\n';
         
         return card;
-    }
-
-    getUpperTitle() {
-        const upperTitle = this.title.toUpperCase();
-
-        return upperTitle;
     }
 }
